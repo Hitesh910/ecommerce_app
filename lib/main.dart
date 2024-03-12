6ca4b670f992ac55a094aadb363556226ca4e120
Hitesh <Hiteshkhatri910@gmail.com>
2024-03-12 11:02:56 +0530

    EcommerseApp

---
+++ lib/main.dart
@@ -0,0 +1,390 @@
+// import 'package:flutter/material.dart';
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: SafeArea(
+//       child: Scaffold(
+//         backgroundColor: Color(0XFF7CB342),
+//         appBar: AppBar(
+//           backgroundColor: Color(0XFF7CB342),
+//           centerTitle: true,
+//           title: Text(
+//             "Shopkee",
+//           ),
+//         ),
+//         body: Align(
+//           child: Container(
+//             //color: Colors.red,
+//             decoration: BoxDecoration(
+//               color: Color(0xff4CAF50),
+//             ),
+//             width: 300,
+//             height: 300,
+//             child: Center(
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   color: Color(0xffb2ff59),
+//                 ),
+//                 width: 250,
+//                 height: 250,
+//                 child: Align(
+//                   alignment: Alignment.center,
+//                   child: Text("oooo",style: TextStyle(
+//                     fontSize: 160,letterSpacing: -50,color: Color(0xff597f2c)
+//                   )),
+//
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   ));
+// }
+
+// import 'package:flutter/material.dart';
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SafeArea(
+//         child: Scaffold(
+//           appBar: AppBar(
+//             backgroundColor: Colors.red,
+//             centerTitle: true,
+//             title: const Text("Mission of RNW"),
+//           ),
+//           body: Align(
+//             child: Container(
+//               decoration: BoxDecoration(
+//                   color: Colors.red.shade100,
+//                   border:
+//                       Border(left: BorderSide(color: Colors.red, width: 20))),
+//               width: 350,
+//               height: 150,
+//               child: Center(
+//                 child: const Text.rich(
+//                   TextSpan(
+//                     children: [
+//                       TextSpan(
+//                         text: 'Shaping"skills"for"scalling"higher\n',
+//                         style: TextStyle(
+//                             fontWeight: FontWeight.bold,
+//                             color: Colors.black,
+//                             fontSize: 20),
+//                       ),
+//                       TextSpan(
+//                         text: '-RNW',
+//                         style: TextStyle(fontSize: 20)
+//                       )
+//                     ],
+//
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+//  }
+
+// import 'package:flutter/material.dart';
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Mix-up"),
+//           centerTitle: true,
+//           backgroundColor: Colors.red,
+//         ),
+//         body: Align(
+//           child: Container(
+//             decoration: BoxDecoration(
+//               color: Colors.blue,
+//             ),
+//             width: 400,
+//             height: 400,
+//             alignment: Alignment.bottomRight,
+//             child: Container(
+//               decoration: BoxDecoration(
+//                 color: Colors.yellow,
+//               ),
+//               width: 320,
+//               height: 340,
+//               alignment: Alignment.bottomRight,
+//               child: Container(
+//                 decoration: BoxDecoration(color: Colors.pink),
+//                 width: 280,
+//                 height: 280,
+//                 alignment: Alignment.topLeft,
+//                 child: Container(
+//                   decoration: BoxDecoration(color: Colors.orange),
+//                   width: 230,
+//                   height: 230,
+//                   alignment: Alignment.topLeft,
+//                   child: Container(
+//                     decoration: BoxDecoration(color: Colors.green),
+//                     width: 180,
+//                     height: 180,
+//                   alignment: Alignment.center,
+//                   child: Container(
+//                     decoration: BoxDecoration(color: Colors.greenAccent),
+//                   width: 130,
+//                   height: 130,),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   ));
+// }
+
+// import 'package:flutter/material.dart';
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Marshal"),
+//           centerTitle: true,
+//           backgroundColor: Colors.brown,
+//         ),
+//         body: Align(
+//           child: Container(
+//             decoration: BoxDecoration(
+//                 color: Colors.brown,
+//                 border: Border(
+//               top: BorderSide(width: 10, color: Colors.brown.shade400),
+//               bottom: BorderSide(width: 10, color: Colors.brown.shade400),
+//               left: BorderSide(width: 30, color: Colors.white),
+//               right: BorderSide(width: 30, color: Colors.white),
+//             )),
+//             width: 100,
+//             height: 100,
+//             alignment: Alignment(0,-4.2),
+//             child: Text("🔥",style: TextStyle(fontSize: 40),),
+//           )
+//         ),
+//       ),
+//     ),
+//   ));
+// }
+
+// import 'package:flutter/material.dart';
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SafeArea(
+//         child: Scaffold(
+//           appBar: AppBar(
+//             title: Text("Lettre Cover"),
+//             backgroundColor: Colors.green,
+//             centerTitle: true,
+//           ),
+//           body: Center(
+//             child: Container(
+//               decoration: BoxDecoration(
+//                 color: Colors.green,
+//                 border: Border(
+//                   top: BorderSide(width: 130,color: Colors.green.shade400),
+//                     right: BorderSide(width: 120,color: Colors.green),
+//                   bottom: BorderSide(width: 130,color: Colors.green.shade400),
+//                   left: BorderSide(width: 120,color: Colors.green),
+//                 ),
+//               ),
+//               width: 300,
+//               height: 300,
+//               alignment: Alignment.center
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// import 'package:flutter/material.dart';
+// void main()
+// {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text("3D cube"),
+//           backgroundColor: Color(0xff009688),
+//           centerTitle: true,
+//         ),
+//         body: Align(
+//            child: Container(
+//              decoration: BoxDecoration(color: Color(0xff009688),
+//              border: Border(left: BorderSide(width: 50,color: Color(0xff33aba0)),
+//              right:BorderSide (width: 50,color: Color(0xff33aba0)),
+//              top: BorderSide(width: 50,color: Color(0xff4db6ac)),
+//              bottom: BorderSide(width: 50,color: Color(0xff4db6ac))),),
+//
+//              width: 300,
+//              height: 300,
+//            ),
+//         ),
+//       ),
+//     ),
+//   ));
+// }
+
+// import 'package:flutter/material.dart';
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Opened Doors"),
+//           backgroundColor: Colors.black,
+//           centerTitle: true,
+//         ),
+//         body: Align(
+//           child: Container(
+//             decoration: BoxDecoration(
+//               color: Colors.black,
+//                 border: Border(
+//               top: BorderSide(width: 50, color: Colors.black),
+//               right: BorderSide(width: 90, color: Color(0xffeeeeee)),
+//               bottom: BorderSide(width: 50, color: Colors.black),
+//               left: BorderSide(width: 90, color: Color(0xffeeeeee)),
+//             )),
+//             width: 300,
+//             height: 300,
+//           ),
+//         ),
+//       ),
+//     ),
+//   ));
+// }
+
+import 'package:flutter/material.dart';
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          floatingActionButton:
+          Container(
+            height: 50,
+            width: 500,
+            color: Colors.yellow,
+            child: Center(
+              child: Text("Emoji" ),
+            ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          appBar: AppBar(
+            title: const Text("Emoji"),
+            //b//ackgroundColor: Colors.yellow,
+            centerTitle: true,
+          ),
+          body: Align(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.orange,
+                shape: BoxShape.circle,
+              ),
+              width: 450,
+              height: 350,
+              alignment: Alignment.center,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                height: 300,
+                width: 350,
+                alignment: Alignment(0,0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  width: 340,
+                  height: 320,
+                   alignment: Alignment(0,-1.5),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      shape: BoxShape.circle,
+                    ),
+                    height: 280,
+                    width: 300,
+                     alignment: Alignment(-0.5,-0.2),
+
+
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                      height: 80,
+                      width: 80,
+                     alignment: Alignment.centerRight,
+
+                      child: Align(
+                        alignment: Alignment(120,0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle
+                          ),
+                          height: 78,
+                          width: 78,
+                        ),
+
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
+// import 'package:flutter/material.dart';
+// void main() {
+//   runApp(
+//      MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SafeArea(
+//         child: Scaffold(
+//           //appBar: BottomAppBar,
+//
+//           bottomNavigationBar: BottomAppBar(
+//             //shadowColor: Colors.yellow,
+//             color: Colors.orange,
+//
+//             // child: Align(
+//             // alignment: Alignment.bottomCenter
+//             //
+//             //   ,  child: Text("bottam"),
+//
+//
+//            // )
+//           ),)
+//         ),)
+//
+//
+//   );}
